0c66d0759ca7de54c97a8ef9a9569eb54975b293
Chris Holland <zrenfire@gmail.com>
2018-10-12 22:37:33 -0400

    Support passing textColor in settitlebarcolor.py
    Needed to fix the "Apply Colors" button in the widget.

--- settitlebarcolor.py
+++ settitlebarcolor.py
@@ -7,19 +7,28 @@ def printHelp():
 	print("python3 settitlebarcolor.py [red],[green],[blue]")
 	print("\t[red/green/blue] are integers from 0-255")
 	print("\tEg: python3 settitlebarcolor.py 0,0,0")
+	print("python3 settitlebarcolor.py [bgColor] [textColor]")
+	print("\tEg: python3 settitlebarcolor.py 0,0,0 239,240,241")
+
+def parseColorStr(colorStr):
+	r, g, b = map(int, colorStr.split(','))
+	return "{},{},{}".format(r, g, b)
 
 if __name__ == '__main__':
-	if len(sys.argv) >= 2:
-		try:
-			r, g, b = map(int, sys.argv[1].split(','))
-			newColor = "{},{},{}".format(r, g, b)
-			setTitlebarColors(newColor)
-		except Exception as e:
-			print(e)
+	try:
+		if len(sys.argv) >= 3:
+			bgColor = parseColorStr(sys.argv[1])
+			textColor = parseColorStr(sys.argv[2])
+			setTitlebarColors(bgColor, textColor)
+		elif len(sys.argv) >= 2:
+			bgColor = parseColorStr(sys.argv[1])
+			setTitlebarColors(bgColor)
+		else:
 			printHelp()
-			sys.exit(0)
-	else:
+			print()
+			print("No color given. Using the default 0,0,0 (black).")
+			setTitlebarColors('0,0,0')
+	except Exception as e:
+		print(e)
 		printHelp()
-		print()
-		print("No color given. Using the default 0,0,0 (black).")
-		setTitlebarColors('0,0,0')
+		sys.exit(0)
